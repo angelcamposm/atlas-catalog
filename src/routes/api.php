dd008d98b31774c7d4210707e95567c697f63758
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-20 23:58:44 +0200

    feat: add `api-statuses` API resource route setup

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ApiStatusController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    //
+    Route::apiResource('api-statuses', ApiStatusController::class);
 });
