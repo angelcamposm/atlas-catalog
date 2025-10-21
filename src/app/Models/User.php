e73581ed63212f2ca2c8c0c885138fc0158eca23
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-21 23:10:34 +0200

    docs: add `firstOrCreate` method annotation to `User` model for improved type hinting

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,13 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @method static firstOrCreate(array $attributes = [], array $values = [], array $joining = [], $touch = true)
+ */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
