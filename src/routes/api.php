b436017184439708ae5119dc9fdfbc37cc4737ca
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-22 23:48:05 +0200

    feat: add API resources for `ApiController`, `BusinessDomainController`, and `LifecycleController`

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\ApiAccessPolicyController;
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\ApiStatusController;
 use App\Http\Controllers\ApiTypeController;
 use App\Http\Controllers\AuthenticationMethodController;
+use App\Http\Controllers\BusinessDomainController;
+use App\Http\Controllers\LifecycleController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -11,6 +14,12 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('api-access-policies', ApiAccessPolicyController::class);
     Route::apiResource('api-statuses', ApiStatusController::class);
     Route::apiResource('api-types', ApiTypeController::class);
+    Route::apiResource('apis', ApiController::class);
+
+    // Business Domain
+    Route::apiResource('business-domains', BusinessDomainController::class);
+    Route::apiResource('lifecycles', LifecycleController::class);
+
     // Security Domain
     Route::apiResource('authentication-methods', AuthenticationMethodController::class);
 });
