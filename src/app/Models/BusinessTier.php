9983a13c2a2b25897186979f1defe05d8e798136
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-26 17:59:00 +0100

    docs: add `code` and `description` properties to `BusinessTier` model docblocks

--- src/app/Models/BusinessTier.php
+++ src/app/Models/BusinessTier.php
@@ -11,6 +11,8 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property int $id
+ * @property string $code
+ * @property string $description
  * @property string $name
  * @property int $created_by
  * @property int $updated_by
