b02157fe353682179c4dc9233ed7fa3d87445cb9
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-27 23:03:22 +0100

    refactor: integrate `HasDataFile` trait and adjust seeder file structure

--- src/database/seeders/GroupTypeSeeder.php
+++ src/database/seeders/GroupTypeSeeder.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\GroupType;
+use App\Traits\HasDataFile;
 use Illuminate\Database\Seeder;
 
 final class GroupTypeSeeder extends Seeder
 {
+    use HasDataFile;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $rows = include database_path('data/group_types.php');
-
-        collect($rows)->each(function ($item) {
+        collect(self::getRows())->each(function ($item) {
             GroupType::updateOrCreate(
                 ['name' => $item['name']],
                 [
