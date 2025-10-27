7bbd769afc5a1d7673f996b7ea62cd3f8bd98e53
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-27 23:03:25 +0100

    refactor: integrate `HasDataFile` trait and adjust seeder file structure

--- src/database/seeders/LifecycleSeeder.php
+++ src/database/seeders/LifecycleSeeder.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\Lifecycle;
+use App\Traits\HasDataFile;
 use Illuminate\Database\Seeder;
 
 final class LifecycleSeeder extends Seeder
 {
+    use HasDataFile;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $rows = include database_path('data/lifecycles.php');
-
-        collect($rows)->each(function ($item) {
+        collect(self::getRows())->each(function ($item) {
             Lifecycle::updateOrCreate(
                 ['name' => $item['name']],
                 [
