c63ef112efc0e3c696ac9ecfd9483b6ce027c71b
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-27 23:03:05 +0100

    refactor: integrate `HasDataFile` trait and adjust seeder file structure

--- src/database/seeders/ClusterTypeSeeder.php
+++ src/database/seeders/ClusterTypeSeeder.php
@@ -6,20 +6,21 @@ namespace Database\Seeders;
 
 use App\Models\ClusterType;
 use App\Models\Vendor;
+use App\Traits\HasDataFile;
 use Illuminate\Database\Seeder;
 
 final class ClusterTypeSeeder extends Seeder
 {
+    use HasDataFile;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $rows = include database_path('data/cluster_types.php');
-
         $vendors = Vendor::all(['id', 'name']);
 
-        collect($rows)->each(function ($item) use ($vendors) {
+        collect(self::getRows())->each(function ($item) use ($vendors) {
 
             $vendor = $vendors->where('name', '=', $item['vendor'])->first();
 
