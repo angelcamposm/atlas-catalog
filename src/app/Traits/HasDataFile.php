087c047488095396b1ef70d659ccc40e655f2b97
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-27 23:01:32 +0100

    feat: add `HasDataFile` trait for handling seed file data retrieval

---
+++ src/app/Traits/HasDataFile.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasDataFile
+{
+    /**
+     * Retrieves rows of data from a seed file in the database path.
+     *
+     * @return array The array of data contained in the specified file.
+     */
+    public static function getRows(): array
+    {
+        return include_once database_path(self::getFilePath());
+    }
+
+    /**
+     * Constructs the full file path for a data file by appending the generated
+     * filename to the predefined database data directory path.
+     *
+     * @return string The full file path for the data file.
+     */
+    private static function getFilePath(): string
+    {
+        return 'data/'.self::getFileName();
+    }
+
+    /**
+     * Generates the filename for the current class by converting its basename
+     * to snake_case and appending the '.php' extension.
+     *
+     * @return string The generated filename in snake_case with a '.php' extension.
+     */
+    private static function getFileName(): string
+    {
+        $class_basename = class_basename(static::class);
+
+        return Str::snake($class_basename).'.php';
+    }
+}
