1520dc4482f79467f7df6c024f2d43600cbdf392
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-27 23:03:13 +0100

    refactor: integrate `HasDataFile` trait and adjust seeder file structure

--- src/database/seeders/FrameworkSeeder.php
+++ src/database/seeders/FrameworkSeeder.php
@@ -6,20 +6,21 @@ namespace Database\Seeders;
 
 use App\Models\Framework;
 use App\Models\ProgrammingLanguage;
+use App\Traits\HasDataFile;
 use Illuminate\Database\Seeder;
 
 final class FrameworkSeeder extends Seeder
 {
+    use HasDataFile;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $rows = include database_path('data/frameworks.php');
-
         $languages = ProgrammingLanguage::all(['id', 'name'])->collect();
 
-        collect($rows)->each(function ($item) use ($languages) {
+        collect(self::getRows())->each(function ($item) use ($languages) {
 
             $language = $languages->where('name', $item['language_name'])->first();
 
