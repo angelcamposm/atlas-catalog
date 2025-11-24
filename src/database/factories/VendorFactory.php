6e53091e1242f560baf04af11f2008e512021685
angel <angel.campos.m@outlook.com>
2025-11-24 19:54:34 +0100

    refactor: Create VendorFactory with default states and relationships

---
+++ src/database/factories/VendorFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Vendor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Vendor>
+ */
+class VendorFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<Vendor>
+     */
+    protected $model = Vendor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->company(),
+            'icon' => $this->faker->word() . '.svg',
+            'url' => $this->faker->url(),
+            'created_by' => User::factory(),
+            'updated_by' => User::factory(),
+        ];
+    }
+}
