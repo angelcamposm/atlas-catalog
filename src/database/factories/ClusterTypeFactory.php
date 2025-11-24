6628a3f2b1de4d6e2152f81140f5ff6dab7261ad
angel <angel.campos.m@outlook.com>
2025-11-24 19:56:21 +0100

    refactor: Create ClusterTypeFactory with default states and relationships

---
+++ src/database/factories/ClusterTypeFactory.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\ClusterType;
+use App\Models\User;
+use App\Models\Vendor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ClusterType>
+ */
+class ClusterTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<ClusterType>
+     */
+    protected $model = ClusterType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word(),
+            'icon' => $this->faker->word() . '.svg',
+            'is_enabled' => $this->faker->boolean(),
+            'vendor_id' => Vendor::factory(),
+            'created_by' => User::factory(),
+            'updated_by' => User::factory(),
+        ];
+    }
+
+    /**
+     * Indicate that the cluster type is enabled.
+     *
+     * @return self
+     */
+    public function enabled(): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_enabled' => true,
+        ]);
+    }
+
+    /**
+     * Indicate that the cluster type is disabled.
+     *
+     * @return self
+     */
+    public function disabled(): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_enabled' => false,
+        ]);
+    }
+}
