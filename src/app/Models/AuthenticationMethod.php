83bb4f57a1949c6e3c13ffd098a370369e3d8608
angel <angel.campos.m@outlook.com>
2025-11-24 12:20:30 +0100

    chore: Add AuthenticationMethodFactory and enable HasFactory trait in AuthenticationMethod model

--- src/app/Models/AuthenticationMethod.php
+++ src/app/Models/AuthenticationMethod.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use App\Observers\AuthenticationMethodObserver;
 use App\Traits\BelongsToUser;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -27,6 +28,7 @@ use Illuminate\Database\Eloquent\Model;
 class AuthenticationMethod extends Model
 {
     use BelongsToUser;
+    use HasFactory;
 
     /**
      * The table associated with the model.
