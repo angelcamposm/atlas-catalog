7119b7d1abc485078e1d535222c7dc705725b75d
angel <angel.campos.m@outlook.com>
2025-11-24 19:53:57 +0100

    refactor: Revamp NodeFactory with enums, refined state methods, and simplified definitions

--- src/database/factories/NodeFactory.php
+++ src/database/factories/NodeFactory.php
@@ -6,6 +6,7 @@ namespace Database\Factories;
 
 use App\Enums\CpuArchitecture;
 use App\Enums\DiscoverySource;
+use App\Enums\NodeType;
 use App\Helpers\MemoryBytes;
 use App\Models\Node;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -16,98 +17,136 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class NodeFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<Node>
+     */
+    protected $model = Node::class;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
-        $host = $this->getHostDetails();
-
-        $cpu = $this->getCpuDetails();
-
-        return [
-            'name' => $host->name,
-            'discovery_source' => $this->getDiscoverySource(),
-            'cpu_architecture' => $cpu->architecture,
-            'cpu_sockets' => $cpu->sockets,
-            'cpu_cores' => $cpu->cores,
-            'cpu_threads' => $cpu->threads,
-            'smt_enabled' => $cpu->smt_enabled,
-            'memory_bytes' => $this->getMemoryBytes(),
-            'hostname' => $host->hostname,
-            'fqdn' => $host->fqdn,
-            'ip_address' => $this->faker->localIpv4(),
-            'mac_address' => $this->faker->macAddress(),
-            'node_type' => $this->faker->randomElements(['H', 'P', 'U', 'V']),
-            'os' => $this->faker->linuxPlatformToken(),
-            'os_version' => $this->faker->semver(),
-            'timezone' => $this->faker->timezone,
-        ];
-    }
-
-    private function getMemoryBytes(): int
-    {
-        return $this->faker->randomElement([
-            MemoryBytes::gigabytes(8),
-            MemoryBytes::gigabytes(16),
-            MemoryBytes::gigabytes(24),
-            MemoryBytes::gigabytes(32),
-            MemoryBytes::gigabytes(48),
-            MemoryBytes::gigabytes(64),
-        ])->value;
-    }
-
-    private function getHostDetails(): object
-    {
-        $name = strtolower($this->faker->domainWord);
-        $domain = $this->faker->domainWord;
-        $tld = $this->faker->tld();
+        $name = strtolower(fake()->domainWord());
+        $domain = fake()->domainWord();
+        $tld = fake()->tld();
         $fqdn = "$name.$domain.$tld";
 
-        return (object) [
+        $cores = fake()->randomElement([2, 4, 8, 16]);
+        $smt_enabled = fake()->boolean();
+        $threads = $smt_enabled ? $cores : 0;
+
+        return [
             'name' => $name,
+            'discovery_source' => fake()->randomElement(DiscoverySource::cases()),
+            'cpu_architecture' => fake()->randomElement(CpuArchitecture::cases()),
+            'cpu_sockets' => fake()->boolean(),
+            'cpu_cores' => $cores,
+            'cpu_threads' => $threads,
+            'smt_enabled' => $smt_enabled,
+            'memory_bytes' => fake()->randomElement([
+                MemoryBytes::gigabytes(8),
+                MemoryBytes::gigabytes(16),
+                MemoryBytes::gigabytes(24),
+                MemoryBytes::gigabytes(32),
+                MemoryBytes::gigabytes(48),
+                MemoryBytes::gigabytes(64),
+            ]),
             'hostname' => $name,
             'fqdn' => $fqdn,
+            'ip_address' => fake()->localIpv4(),
+            'mac_address' => fake()->macAddress(),
+            'node_type' => fake()->randomElement(NodeType::cases()),
+            'os' => fake()->linuxPlatformToken(),
+            'os_version' => fake()->semver(),
+            'timezone' => fake()->timezone(),
         ];
     }
 
-    private function getCpuCores(): int
+    /**
+     * Indicate that the node is a hybrid node.
+     *
+     * @return self
+     */
+    public function hybrid(): self
     {
-        return $this->faker->randomElement([2, 4, 8, 16]);
+        return $this->state(fn (array $attributes) => [
+            'node_type' => NodeType::Hybrid,
+        ]);
     }
 
-    private function getCpuDetails(): object
+    /**
+     * Indicate that the node is a physical node.
+     *
+     * @return self
+     */
+    public function physical(): self
     {
-        $cores = $this->getCpuCores();
-        $smt_enabled = $this->faker->boolean();
-
-        if ($smt_enabled) {
-            return (object) [
-                'architecture' => CpuArchitecture::X86_64->value,
-                'cores' => $cores,
-                'smt_enabled' => true,
-                'sockets' => $this->faker->boolean(),
-                'threads' => $cores,
-            ];
-        }
-
-        return (object) [
-            'architecture' => $this->getCpuArchitecture(),
-            'cores' => $cores,
-            'smt_enabled' => false,
-            'sockets' => 1,
-            'threads' => 0,
-        ];
+        return $this->state(fn (array $attributes) => [
+            'node_type' => NodeType::Physical,
+        ]);
     }
 
-    public function getCpuArchitecture(): string
+    /**
+     * Indicate that the node is an unknown node.
+     *
+     * @return self
+     */
+    public function unknown(): self
     {
-        return $this->faker->randomElement(CpuArchitecture::cases())->value;
+        return $this->state(fn (array $attributes) => [
+            'node_type' => NodeType::Unknown,
+        ]);
     }
 
-    private function getDiscoverySource(): string
+    /**
+     * Indicate that the node is a virtual node.
+     *
+     * @return self
+     */
+    public function virtual(): self
     {
-        return $this->faker->randomElement(DiscoverySource::cases())->value;
+        return $this->state(fn (array $attributes) => [
+            'node_type' => NodeType::Virtual,
+        ]);
+    }
+
+    /**
+     * Indicate that the node was discovered manually.
+     *
+     * @return self
+     */
+    public function manual(): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'discovery_source' => DiscoverySource::Manual,
+        ]);
+    }
+
+    /**
+     * Indicate that the node was discovered through a CI-CD pipeline.
+     *
+     * @return self
+     */
+    public function pipeline(): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'discovery_source' => DiscoverySource::Pipeline,
+        ]);
+    }
+
+    /**
+     * Indicate that the node was discovered by running a cluster scan.
+     *
+     * @return self
+     */
+    public function scan(): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'discovery_source' => DiscoverySource::Scan,
+        ]);
     }
 }
