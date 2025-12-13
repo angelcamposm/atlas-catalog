2aa53cc3646791f7d34b3f0e2847fdb85fe72b90
angel <angel.campos.m@outlook.com>
2025-12-13 16:53:26 +0100

    feat: Add BusinessCapability model, migration, requests, resources, factory, policy, observer, and controller

---
+++ src/app/Models/BusinessCapability.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\Observers\BusinessCapabilityObserver;
+use App\Traits\BelongsToUser;
+use App\Traits\HasRelatives;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property int $parent_id
+ * @property int $strategic_value
+ * @property int $created_by
+ * @property int $updated_by
+ * @property-read User|null $creator The user who created this language entry.
+ * @property-read User|null $updater The user who last updated this language entry.
+ *
+ * @method static create(array $validated)
+ * @method static firstOrCreate(array $attributes = [], array $values = [])
+ * @method static inRandomOrder()
+ * @method static paginate()
+ * @method static pluck(string $string)
+ * @method static updateOrCreate(array $attributes = [], array $values = [])
+ */
+#[ObservedBy(BusinessCapabilityObserver::class)]
+class BusinessCapability extends Model
+{
+    use BelongsToUser;
+    use HasFactory;
+    use HasRelatives;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string|null
+     */
+    protected $table = 'business_capabilities';
+
+    /**
+     * The attributes that are mass-assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'parent_id',
+        'strategic_value',
+        'created_by',
+        'updated_by',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<string>
+     */
+    protected $hidden = [
+        //
+    ];
+}
---
+++ src/database/migrations/2025_11_03_213606_create_business_capabilities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_capabilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('description', 255)->nullable();
+            $table->tinyInteger('strategic_value')->nullable()->comment('How critical is this to the business?');
+            $table->foreignId('parent_id')->nullable()->references('id')->on('business_capabilities')->nullOnDelete();
+            $table->timestamp('created_at')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users', 'id')->nullOnDelete();
+            $table->timestamp('updated_at')->nullable();
+            $table->foreignId('updated_by')->nullable()->constrained('users', 'id')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_capabilities');
+    }
+};
