0210d1bddaee44c9b8be0951ad395e8cd5ff1a7b
angel <angel.campos.m@outlook.com>
2025-12-14 23:11:44 +0100

    feat: Add `GroupMemberFactory` to streamline `GroupMember` test data generationfeat: Add factories for `GroupType` and `GroupMemberRole` to streamline test data generation

---
+++ src/database/factories/GroupTypeFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\GroupType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<GroupType>
+ */
+class GroupTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<GroupType>
+     */
+    protected $model = GroupType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->name(),
+            'description' => $this->faker->text(250),
+        ];
+    }
+}
