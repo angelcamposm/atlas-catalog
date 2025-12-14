5aab4eae55148125de0fe803c8e6660365c2673d
angel <angel.campos.m@outlook.com>
2025-12-14 23:10:39 +0100

    refactor: Simplify `atlas:generate-fake-data` command by delegating to specific fake data commands

--- src/app/Console/Commands/GenerateFakeDataCommand.php
+++ src/app/Console/Commands/GenerateFakeDataCommand.php
@@ -1,14 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands;
 
-use App\Enums\DiscoverySource;
-use App\Models\BusinessCapability;
-use App\Models\Component;
-use App\Models\System;
 use Illuminate\Console\Command;
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(name: 'atlas:generate-fake-data')]
@@ -19,9 +15,7 @@ class GenerateFakeDataCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'atlas:generate-fake-data
-        { --Q|quantity=100 : How many records must be generated }
-        ';
+    protected $signature = 'atlas:generate-fake-data';
 
     /**
      * The console command description.
@@ -50,53 +44,11 @@ class GenerateFakeDataCommand extends Command
     public function handle(): int
     {
         $this->info('Generating fake data...');
-        $this->create_clusters_fake_data();
-        $this->create_groups_fake_data();
-        $this->create_systems_fake_data();
+
+        $this->call(GenerateClustersFakeDataCommand::class, ['--quantity' => 10]);
+        $this->call(GenerateGroupsFakeDataCommand::class, ['--quantity' => 10]);
+        $this->call(GenerateSystemsFakeDataCommand::class, ['--quantity' => 10]);
 
         return self::SUCCESS;
-    }
-
-    private function getComponentFactory(): Factory
-    {
-        return Component::factory()
-            ->count(1)
-            ->state(new Sequence(
-                ['discovery_source' => DiscoverySource::Manual],
-                ['discovery_source' => DiscoverySource::Pipeline],
-                ['discovery_source' => DiscoverySource::Scan],
-            ))
-            ->state(new Sequence(
-                ['is_stateless' => true],
-                ['is_stateless' => false],
-            ))
-            ->state(new Sequence(
-                ['is_exposed' => true],
-                ['is_exposed' => false],
-            ));
-    }
-
-    public function create_systems_fake_data(): void
-    {
-        $this->info('Creating fake data for systems domain...');
-
-        $components = $this->getComponentFactory();
-
-        System::factory($this->option('quantity'))
-            ->has($components, 'components')
-            ->has(BusinessCapability::factory(), 'businessCapabilities')
-            ->create();
-    }
-
-    private function create_clusters_fake_data(): void
-    {
-        //TODO: implement this method
-        $this->info('Creating fake data for clusters domain...');
-    }
-
-    private function create_groups_fake_data(): void
-    {
-        //TODO: implement this method
-        $this->info('Creating fake data for groups domain...');
     }
 }
