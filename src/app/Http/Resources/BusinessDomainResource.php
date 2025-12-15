87d620beec8cd43aa1b15ad71bff270c981e3149
angel <angel.campos.m@outlook.com>
2025-12-15 23:00:09 +0100

    feat: Extend `BusinessDomainResource` with detailed attributes and category structure

--- src/app/Http/Resources/BusinessDomainResource.php
+++ src/app/Http/Resources/BusinessDomainResource.php
@@ -4,9 +4,24 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+use App\Enums\BusinessDomainCategory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property BusinessDomainCategory $category
+ * @property string $description
+ * @property string $display_name
+ * @property boolean $is_active
+ * @property string $name
+ * @property int $parent_id
+ * @property Carbon $created_at
+ * @property int $created_by
+ * @property Carbon $updated_at
+ * @property int $updated_by
+ */
 class BusinessDomainResource extends JsonResource
 {
     /**
@@ -16,6 +31,21 @@ class BusinessDomainResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'display_name' => $this->display_name,
+            'description' => $this->description,
+            'category' => [
+                'name' => $this->category->name,
+                'value' => $this->category->value,
+            ],
+            'is_active' => $this->is_active,
+            'parent_id' => $this->parent_id,
+            'created_at' => $this->created_at,
+            'created_by' => $this->created_by,
+            'updated_at' => $this->updated_at,
+            'updated_by' => $this->updated_by,
+        ];
     }
 }
