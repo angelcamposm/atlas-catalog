0021a4f003bb027cd87672be7b1201c12bffde99
angel <angel.campos.m@outlook.com>
2025-12-18 19:26:50 +0100

    feat: Add `BusinessCapabilitySystemController` to handle paginated systems for a business capabilityfeat: Add controllers for paginated business domain components and business capability systems

---
+++ src/app/Http/Controllers/SystemComponentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ComponentResourceCollection;
+use App\Models\System;
+use Illuminate\Http\Request;
+
+class SystemComponentController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request, System $system): ComponentResourceCollection
+    {
+        $components = $system->components()->paginate();
+
+        return new ComponentResourceCollection($components);
+    }
+}
