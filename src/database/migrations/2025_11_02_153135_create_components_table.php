ec8c4ce4fa38c4938dda0b17cb4c169d410a001f
angel <angel.campos.m@outlook.com>
2025-12-18 20:18:23 +0100

    feat: Update `components` table to support lifecycle phases and add end-of-life date

--- src/database/migrations/2025_11_02_153135_create_components_table.php
+++ src/database/migrations/2025_11_02_153135_create_components_table.php
@@ -23,7 +23,8 @@ return new class extends Migration
             $table->boolean('has_zero_downtime_deployments')->default(false);
             $table->boolean('is_exposed')->default(false);
             $table->boolean('is_stateless')->default(true);
-            $table->foreignId('lifecycle_id')->nullable()->constrained('lifecycles', 'id')->nullOnDelete();
+            $table->foreignId('lifecycle_id')->nullable()->constrained('lifecycle_phases', 'id')->nullOnDelete();
+            $table->date('end_of_life_at')->nullable();
             $table->foreignId('owner_id')->nullable()->constrained('groups', 'id')->nullOnDelete();
             $table->foreignId('platform_id')->nullable()->constrained('platforms', 'id')->nullOnDelete();
             $table->string('slug')->unique();
