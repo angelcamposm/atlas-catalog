8b97d3c3a531b6a07f7d136768e48a6f6f960aca
angel <angel.campos.m@outlook.com>
2025-12-18 19:24:42 +0100

    feat: Add `AllowedRelationships` trait for filtering and validating eager-loading relationships

---
+++ src/app/Http/Controllers/ApiComponentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ComponentResourceCollection;
+use App\Models\Api;
+use Illuminate\Http\Request;
+
+class ApiComponentController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request, Api $api): ComponentResourceCollection
+    {
+        $components = $api->components()->paginate();
+
+        return new ComponentResourceCollection($components);
+    }
+}
