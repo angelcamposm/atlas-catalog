c9237e3c45bf091243a46a9bc8ee4900a92434c2
angel <angel.campos.m@outlook.com>
2025-12-22 10:41:58 +0100

    docs: Fix typo in `Category` model docblock

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -47,7 +47,7 @@ class Category extends Model
     protected $table = 'categories';
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass-assignable.
      *
      * @var array<int, string>
      */
