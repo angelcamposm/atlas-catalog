d0292c75290af1481a9a3fad7e8fa3d582442e6b
angel <angel.campos.m@outlook.com>
2025-12-23 11:56:53 +0100

    feat: Add support for configurable relationships in `BusinessDomainEntityController` using `AllowedRelationships` trait

--- src/app/Http/Controllers/BusinessDomainEntityController.php
+++ src/app/Http/Controllers/BusinessDomainEntityController.php
@@ -1,19 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Http\Resources\EntityResourceCollection;
 use App\Models\BusinessDomain;
+use App\Traits\AllowedRelationships;
 use Illuminate\Http\Request;
 
 class BusinessDomainEntityController extends Controller
 {
+    use AllowedRelationships;
+
+    public const array ALLOWED_RELATIONSHIPS = [
+        'creator',
+        'updater',
+    ];
+
     /**
      * Handle the incoming request.
+     *
+     * @param  Request         $request
+     * @param  BusinessDomain  $businessDomain
+     *
+     * @return EntityResourceCollection
      */
     public function __invoke(Request $request, BusinessDomain $businessDomain): EntityResourceCollection
     {
-        $components = $businessDomain->entities()->paginate();
+        $requestedRelationships = $request->has('with')
+            ? self::filterAllowedRelationships($request->get('with'))
+            : [];
+
+        $components = $businessDomain->entities()->with($requestedRelationships)->paginate();
 
         return new EntityResourceCollection($components);
     }
