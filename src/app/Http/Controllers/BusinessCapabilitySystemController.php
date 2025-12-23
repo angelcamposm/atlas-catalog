aac66093bdde7a273da5a47e1f98d49236706d5c
angel <angel.campos.m@outlook.com>
2025-12-23 11:56:18 +0100

    feat: Add support for configurable relationships in `BusinessCapabilitySystemController` using `AllowedRelationships` trait

--- src/app/Http/Controllers/BusinessCapabilitySystemController.php
+++ src/app/Http/Controllers/BusinessCapabilitySystemController.php
@@ -6,16 +6,39 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\BusinessCapabilityResourceCollection;
 use App\Models\BusinessCapability;
+use App\Traits\AllowedRelationships;
 use Illuminate\Http\Request;
 
 class BusinessCapabilitySystemController extends Controller
 {
+    use AllowedRelationships;
+
+    /**
+     * List of allowed relationships that can be eagerly loaded for BusinessCapability resources.
+     * These relationships can be included in API responses by passing them via the 'with' query parameter.
+     * Available relationships:
+     *   - components: Components associated with this business capability
+     *   - creator: User who created the business capability
+     *   - updater: User who last updated the business capability
+     *
+     * @var array<int, string>
+     */
+    public const array ALLOWED_RELATIONSHIPS = [
+        'components',
+        'creator',
+        'updater',
+    ];
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request, BusinessCapability $businessCapability): BusinessCapabilityResourceCollection
     {
-        $components = $businessCapability->systems()->paginate();
+        $requestedRelationships = $request->has('with')
+            ? self::filterAllowedRelationships($request->get('with'))
+            : [];
+
+        $components = $businessCapability->systems()->with($requestedRelationships)->paginate();
 
         return new BusinessCapabilityResourceCollection($components);
     }
