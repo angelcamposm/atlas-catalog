ffa35149cc2d7b4105c305973a7152273c6dfe93
angel <angel.campos.m@outlook.com>
2025-12-24 10:21:15 +0100

    refactor: Add `ComponentTypeFactory` and integrate `HasFactory` into `ComponentType` model

--- src/app/Models/ComponentType.php
+++ src/app/Models/ComponentType.php
@@ -6,7 +6,9 @@ namespace App\Models;
 
 use App\Observers\ComponentTypeObserver;
 use App\Traits\BelongsToUser;
+use Database\Factories\ComponentTypeFactory;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -21,11 +23,14 @@ use Illuminate\Database\Eloquent\Model;
  * @method static paginate()
  * @method static pluck(string $string)
  * @method static updateOrCreate(array $attributes = [], array $values = [])
+ *
+ * @use HasFactory<ComponentTypeFactory>
  */
 #[ObservedBy(ComponentTypeObserver::class)]
 class ComponentType extends Model
 {
     use BelongsToUser;
+    use HasFactory;
 
     /**
      * The table associated with the model.
---
+++ src/database/factories/ComponentTypeFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\ComponentType;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<ComponentType>
+ */
+class ComponentTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<ComponentType>
+     */
+    protected $model = ComponentType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word(),
+            'description' => Str::limit($this->faker->sentence(), 255),
+            'icon' => $this->faker->word(),
+        ];
+    }
+}
