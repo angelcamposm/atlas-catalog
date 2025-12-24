6f68d6f590ac6fbeb5120f6b0caac3295be0782b
angel <angel.campos.m@outlook.com>
2025-12-24 22:28:15 +0100

    refactor: add `created_by` and `updated_by` to `users` table migration

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users', 'id')->nullOnDelete();
+            $table->timestamp('updated_at')->nullable();
+            $table->foreignId('updated_by')->nullable()->constrained('users', 'id')->nullOnDelete();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
